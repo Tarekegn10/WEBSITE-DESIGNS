2178a329786dbad0c84a3cb297eb5a1eb187deed
TAREKEGN TADELE <118010414+0947756395@users.noreply.github.com>
2023-09-17 11:01:58 +0300

    Game shop

---
+++ shop.js
@@ -0,0 +1,65 @@
+const shopgames=[
+		{ "title":"Death standing",
+			"price": "13.6$",
+			"Rating": "8/10",
+			"description": "Death Stranding is an action game set in an open world, and includes asynchronous online functions. Kojima refers to Death Stranding as the first \"strand game\", an original genre characterized by the game's incorporation of social elements."
+		},
+		
+		{
+			"title":"Assasin creed",
+			"price": "19.9$",
+			"Rating": "7.5/10",
+			"description": "Assassin's Creed is a non-linear action-adventure video game, during which the player controls a 12th-century Levantine Assassin named Altaïr Ibn-La'Ahad during the Third Crusade, whose life is experienced through the Animus by his 21st century descendant, Desmond Miles."
+		},
+		{
+			"title": "spiderman into multverse",
+			"price": "23.9$",
+			"Rating": "9.5/10",
+			"description": "Marvel's Spider-Man is a series of action-adventure video games developed by Insomniac Games and published by Sony Interactive Entertainment for PlayStation consoles and Microsoft Windows."
+		},
+		{
+			"title":"vikinge",
+			"price":"7.9$",
+			"Rating": "6.5/10",
+			"description":"If you are a gamer who also happens to be a big fan of Norse culture and mythology, you may have been looking for video games that fit the criteria."
+		},
+		{
+			"title":"God of wars",
+			"price": "27.9$",
+			"Rating": "9.8/10",
+			"description":"Kratos eventually finds Pandora's Box, and after finally killing Ares, he ascends to Mount Olympus to become the new God of War."
+		},
+		{
+			"title":"speed race",
+			"price":"10.9$",
+			"Rating":"7/10",
+			"description":"Speed Race is a one or two-player racing video game where players controls a race car along a constantly-scrolling vertical road. The player uses a steering wheel to move the car left or right and an accelerator to make it move faster."
+		},
+		{
+			"title":"kirby",
+			"price":"10.4$",
+			"Rating":"9.8/10",
+			"description":"Kirby has been described as one of the most legendary video game characters of all time by PC World. In virtually all his appearances, Kirby is depicted as cheerful, innocent and food-loving; however, he becomes fearless, bold, and clever in the face of danger."
+		},
+		{
+			"title":"Red Dead Redemption",
+			"price":"25$",
+			"Rating":"9.7/10",
+			"description":"Western-themed action-adventure game played from a third-person perspective. The player controls John Marston and completes missions—linear scenarios with set objectives—to progress through the story; in the game's epilogue, the player controls John's son Jack."
+		},
+		{
+			"title": "Zelda",
+			"price": "22.9$",
+			"Rating": "9.3",
+			"description": "a courageous young man of the elf-like Hylian race, and Princess Zelda, a magical princess who is the mortal reincarnation of the goddess Hylia, as they fight to save the magical land of Hyrule from Ganon, an evil warlord turned demon king"
+		},
+		{
+			"title": "super mario bros",
+			"price": "20$",
+			"Rating": "9/10",
+			"description": "The main hero of the Mushroom Kingdom. Mario is always bright and cheerful and instantly recognizable with his blue overalls, red cap, and trademark moustache. He's a trusted friend of Princess Peach, and he and his brother Luigi are known across the land for their acts of bravery."
+		}
+]
+
+
+console.log(shopgames.length)
